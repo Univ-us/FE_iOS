2f16b93370889233b441b08d99c783ede3a129d7
Sangwoo Byeon <tkddn0518@naver.com>
2023-12-03 00:20:51 +0900

    [Feat] 🎉 프로젝트 생성

---
+++ Univus/UnivusApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnivusApp.swift
+//  Univus
+//
+//  Created by 변상우 on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UnivusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
